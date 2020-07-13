14011670d059e59193bb6b2cbe2141ed8cc94ed6
hyeonQyu <dhk0561@naver.com>
2020-07-13 15:18:52 +0900

    웹 어플리케이션 감시 : ServletContextListener

--- ex/src/com/nextwin/ex/InitParam.java
+++ ex/src/com/nextwin/ex/InitParam.java
@@ -31,6 +31,8 @@ public class InitParam extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		
 		String id = getInitParameter("id");
 		String pw = getInitParameter("pw");
 		
